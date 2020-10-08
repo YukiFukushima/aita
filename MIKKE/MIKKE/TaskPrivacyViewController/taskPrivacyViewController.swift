828c340b3e0845db22cfacb667cc1d1485dd4a1c
福島悠樹 <kybcr376@yahoo.co.jp>
2020-10-08 09:38:08 +0900

    Ver1.0.6(Apple提出五回目)

--- MIKKE/MIKKE/TaskPrivacyViewController/taskPrivacyViewController.swift
+++ MIKKE/MIKKE/TaskPrivacyViewController/taskPrivacyViewController.swift
@@ -9,16 +9,22 @@
 import UIKit
 import WebKit
 
-class taskPrivacyViewController: UIViewController {
+class taskPrivacyViewController: UIViewController, WKNavigationDelegate {
 
     @IBOutlet weak var wkWebView: WKWebView!
+    @IBOutlet weak var backBtnLabel: UIButton!
+    @IBOutlet weak var loadingMessageLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //delegateセット
+        wkWebView.navigationDelegate = self
+        
         //利用規約表示
         self.loadWebView(urlString:"https://yukifukushima.github.io/aitaPolicy/")
+        
         //スワイプで進む、戻るを有効にする
         self.wkWebView.allowsBackForwardNavigationGestures = true
     }
@@ -31,6 +37,55 @@ class taskPrivacyViewController: UIViewController {
         self.wkWebView.load(myReq)
     }
     
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("status")
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Start")
+        
+        //ボタンをまだ押せないようにする
+        backBtnLabel.isUserInteractionEnabled = false
+        backBtnLabel.layer.borderColor = UIColor.darkGray.cgColor
+        backBtnLabel.backgroundColor = .lightGray
+        
+        //メッセージを消す
+        loadingMessageLabel.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finish")
+        
+        //ボタンを押せるようにする
+        backBtnLabel.isUserInteractionEnabled = true
+        backBtnLabel.layer.borderColor = UIColor.systemTeal.cgColor
+        backBtnLabel.backgroundColor = .clear
+        
+        //メッセージを出す
+        loadingMessageLabel.isHidden = true
+        
+        /* ForDebug *
+        self.showAlert(title: "読み込みに失敗しました", message: "アプリを立ち上げ直してください")
+        * EndForDebug */
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("Err!")
+        
+        self.showAlert(title: "読み込みに失敗しました", message: "アプリを立ち上げ直してください")
+    }
+    
+    //アラート表示関数
+    func showAlert( title:String, message:String? ){
+        //UIAlertControllerを関数の引数であるとtitleとmessageを使ってインスタンス化
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        //UIAlertActionを追加
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        //表示
+        self.present(alertVC, animated: true, completion: nil)
+    }
     
     @IBAction func tappedBackBtn(_ sender: Any) {
         dismiss(animated: true, completion: nil)
--- MIKKE/MIKKE/TaskTutorialViewController/PrivacyTutorialViewController.swift
+++ MIKKE/MIKKE/TaskTutorialViewController/PrivacyTutorialViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 import WebKit
 
-class PrivacyTutorialViewController: UIViewController {
+class PrivacyTutorialViewController: UIViewController, WKNavigationDelegate {
 
     @IBOutlet weak var wkWebView: WKWebView!
+    @IBOutlet weak var tappedStartBtnLabel: UIButton!
+    @IBOutlet weak var loadingMessageLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //delegateセット
+        wkWebView.navigationDelegate = self
+        
+        
         //利用規約表示
         self.loadWebView(urlString:"https://yukifukushima.github.io/aitaPolicy/")
+        
         //スワイプで進む、戻るを有効にする
         self.wkWebView.allowsBackForwardNavigationGestures = true
     }
@@ -30,6 +37,56 @@ class PrivacyTutorialViewController: UIViewController {
         self.wkWebView.load(myReq)
     }
 
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("status")
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Start")
+        
+        //ボタンをまだ押せないようにする
+        tappedStartBtnLabel.isUserInteractionEnabled = false
+        tappedStartBtnLabel.layer.borderColor = UIColor.darkGray.cgColor
+        tappedStartBtnLabel.backgroundColor = .lightGray
+        
+        //メッセージを出す
+        loadingMessageLabel.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finish")
+        
+        //ボタンを押せるようにする
+        tappedStartBtnLabel.isUserInteractionEnabled = true
+        tappedStartBtnLabel.layer.borderColor = UIColor.systemTeal.cgColor
+        tappedStartBtnLabel.backgroundColor = .clear
+        
+        //メッセージを消す
+        loadingMessageLabel.isHidden = true
+        
+        /* ForDebug *
+        self.showAlert(title: "読み込みに失敗しました", message: "アプリを立ち上げ直してください")
+        * EndForDebug */
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print("Err!")
+        
+        self.showAlert(title: "読み込みに失敗しました", message: "アプリを立ち上げ直してください")
+    }
+    
+    //アラート表示関数
+    func showAlert( title:String, message:String? ){
+        //UIAlertControllerを関数の引数であるとtitleとmessageを使ってインスタンス化
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        //UIAlertActionを追加
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        //表示
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
     @IBAction func tappedStartBtn(_ sender: Any) {
         //UserDefaultsに保存(最初のログイン時にフラグを落とす。ココでは落とさない。)
         //TutorialViewCompleteRepository.saveTutorialViewCompleteDefaults(tutorialComplete: true)
